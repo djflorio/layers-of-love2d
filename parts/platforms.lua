613d044ee2cbee9669b4ca00b9f0d3fa90f49c8d
Dan Florio <dflo@danflorio.com>
2018-05-26 12:37:13 -0400

    move hole size to globals

--- parts/platforms.lua
+++ parts/platforms.lua
@@ -2,17 +2,13 @@ local globals = require 'globals'
 
 local Platform = require 'parts.Platform'
 
-local settings = {
-    holeSize = 100
-}
-
 local platforms = {}
 
 local function addPlatform()
     local topY = platforms[#platforms].p1.y
     local w = love.graphics.getWidth()
-    local start = love.math.random(10, w - settings.holeSize)
-    platforms[#platforms+1] = Platform(world, topY - 200, start, settings.holeSize)
+    local start = love.math.random(10, w - globals.holeSize)
+    platforms[#platforms+1] = Platform(world, topY - 200, start, globals.holeSize)
     if (#platforms > 5) then
         table.remove(platforms, 1)
     end
@@ -23,7 +19,7 @@ end
 platforms.init = function(world)
     for i=1, 2 do
         local w = love.graphics.getWidth()
-        local hSize = settings.holeSize
+        local hSize = globals.holeSize
         local hStart = love.math.random(10, w - hSize)
         local y = love.graphics.getHeight() - (200 * i)
         platforms[i] = Platform(world, y, hStart, hSize)
